b805a04ceb06ac1e318dcf675f8eaa591149fd44
Mitchell Johnson <orangutandevelopment@gmail.com>
2016-08-12 22:09:07 +1000

    Change Semicolon

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-$('html').css('background', 'red');
-console.log('test');
+//$('html').css('background', 'red');
+String.prototype.replaceAll = function(search, replacement) {
+    return this.split(search).join(replacement);
+};
+function replaceText() {
+    $('pre').each(function(i, el) {
+        $(this).html($(this).html().replaceAll(';', ';'));
+    });
+}
+replaceText();
