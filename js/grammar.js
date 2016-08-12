0ddf58156f1f0133f9bb6ba2c9c7244f35974fa1
Simon Gordon <gordonsimon@hotmail.com>
2016-08-13 01:17:11 +1000

    Lots of work to the grammar tool

--- js/grammar.js
+++ js/grammar.js
@@ -1,13 +1,32 @@
+
+// [original, new, frequency (optional)] 
 var replaceTerms = [
-["it's", "its"],
-["you're", "your"],
-["their", "there"],
-[";", ";"]
+    ["it's", "its"],
+    ["you're", "your"],
+    ["their", "there"],
+    ["you'll", "youll"],
+    // ["Hillary", "Donald"],
+    // ["Clinton", "Trump"],
+    ["Donald Trump", "Donald Duck", 1.0],
+    ["Lorem", "Ipsum", 0.01],
+    ["Microsoft", "Apple", 0.01]
+    // [";", ";"]
 ]
 
-for (var i = 0; i < replaceTerms.length; i++){
-    var curTerms = replaceTerms[i];
-    $("body").html($("body").html().replace(curTerms[0], curTerms[1]));
-}
+//Floating point number from 0 to 1 representing what % of the time this should show up.
+var defaultFreq = 0.1;
+// defaultFreq = 1.0;
 
- 
+console.log("grammar start");
+$("p, li").each(function(){
+    for (var i = 0; i < replaceTerms.length; i++){
+        var freq = replaceTerms[i][2] || defaultFreq;
+        if (Math.random() <= freq){
+            var oldTerm = replaceTerms[i][0];
+            var newTerm = replaceTerms[i][1];
+            // Currently will only replace first instance in each tag, but should be good enough.
+            this.innerHTML = this.innerHTML.replace(oldTerm, newTerm);
+        }
+    }
+});
+console.log("grammar end");
