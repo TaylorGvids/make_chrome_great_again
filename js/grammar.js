430b6acab3ba195b557e1e8232b7974924f1067c
Simon Gordon <gordonsimon@hotmail.com>
2016-08-13 01:44:55 +1000

    improved code, need sleep

--- js/grammar.js
+++ js/grammar.js
@@ -1,13 +1,15 @@
-
 // [original, new, frequency (optional)] 
 var replaceTerms = [
     ["it's", "its"],
     ["you're", "your"],
     ["their", "there"],
+    ["they're", "there"],
     ["you'll", "youll"],
+    ["where", "were"],
     // ["Hillary", "Donald"],
     // ["Clinton", "Trump"],
-    ["Donald Trump", "Donald Duck", 1.0],
+    // ["Donald Trump", "Donald Duck", 1.0],
+    [/Donald J?(ohn)?\.?Trump/, "Donald Duck", 1.0],
     ["Lorem", "Ipsum", 0.01],
     ["Microsoft", "Apple", 0.01]
     // [";", ";"]
@@ -18,15 +20,10 @@ var defaultFreq = 0.1;
 // defaultFreq = 1.0;
 
 console.log("grammar start");
-$("p, li").each(function(){
-    for (var i = 0; i < replaceTerms.length; i++){
-        var freq = replaceTerms[i][2] || defaultFreq;
-        if (Math.random() <= freq){
-            var oldTerm = replaceTerms[i][0];
-            var newTerm = replaceTerms[i][1];
-            // Currently will only replace first instance in each tag, but should be good enough.
-            this.innerHTML = this.innerHTML.replace(oldTerm, newTerm);
-        }
+$("p, li").each(function() {
+    for (var i = 0; i < replaceTerms.length; i++) {
+        if (Math.random() <= (replaceTerms[i][2] || defaultFreq))
+            this.innerHTML = this.innerHTML.replace(replaceTerms[i][0], replaceTerms[i][1]); // Currently will only replace first instance in each tag, but should be good enough.
     }
 });
 console.log("grammar end");
