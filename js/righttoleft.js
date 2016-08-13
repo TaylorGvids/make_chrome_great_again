b4caaa5bef2ba73da063e41e0bda71f658de7273
Simon Gordon <gordonsimon@hotmail.com>
2016-08-13 11:44:35 +1000

    Improved RtL

--- js/righttoleft.js
+++ js/righttoleft.js
@@ -2,13 +2,16 @@
 var defaultFreq = 0.1;
 defaultFreq = 1;
 $("p").each(function(){
-    if (Math.random() <= defaultFreq){
-        var text = $(this).text();
-        var reversed = text.split("").reverse().join("");
-        $(this).text(reversed);
-        // $(this).attr("dir", "rtl");
-        $(this).css("direction", "rtl");
-        $(this).css("unicode-bidi", "bidi-override");
-        $(this).css("text-align", "left");
+    //If the only child of this is text (so it doesn't break tags underneath)
+    if ($(this).contents().length == 1 && $(this).contents()[0].nodeType == 3){
+        if (Math.random() <= defaultFreq){
+            var text = $(this).text();
+            var reversed = text.split("").reverse().join("");
+            $(this).text(reversed);
+            // $(this).attr("dir", "rtl");
+            $(this).css("direction", "rtl");
+            $(this).css("unicode-bidi", "bidi-override");
+            $(this).css("text-align", "left");
+        }
     }
 });
