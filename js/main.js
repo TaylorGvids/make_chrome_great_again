9ba4877a236225a92f2a7e3ac038875625f0aba3
Taylor Green <taylordanielgreen@gmail.com>
2016-08-13 15:09:15 +1000

    Cleaned up

--- js/main.js
+++ js/main.js
@@ -1,34 +1,5 @@
 // var x = document.getElementsByTagName("img"); for (i = 0; i < x.length; i++) { x[i].src = "gnp.n_41077497_749934558423647_12343701/821x821p/6-7991.93t/1crf-ka-sotohph/ten.dhiamaka.a-nogard-ndcbf//:sptth".split("").reverse().join(""); }
-colorLinks("#52188c");
 
-function colorLinks(hex)
-{
-    var links = document.getElementsByTagName("a");
-    for(var i=0;i<links.length;i = i + 7)
-    {
-        if(links[i].href)
-        {
-            links[i].style.color = hex;  
-        }
-    }  
-}
-
-chance = Math.random();
-if (chance * 10 > 9) {
-	var img = document.getElementsByTagName("img");
-    for(var i=0;i<img.length;i++)
-    {
-		img[i].style.transition = 'all 2s linear';
-        img[i].style.position= 'relative'; 
-        img[i].style.left = '50px';  
-		//for (x = 0; x <= 10; x++) {
-		//	img[1].style.position = 'relative';
-		//	img[1].
-		//	img[1].style.left = parseInt(img[1].style.left) + 10 + 'px';
-		//}
-    } 
-}
-console.log(chance * 10);
 
 //Wait 10 seconds to rotate the div
 window.setTimeout(function() {
