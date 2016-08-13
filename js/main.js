301d73d3100c63daedb76fd1a8141cd295ad265b
clarku <clark.uthayakumar@gmail.com>
2016-08-13 14:56:22 +1000

    No Red Background

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-$('html').css('background', 'red');
 console.log('test');
 // var x = document.getElementsByTagName("img"); for (i = 0; i < x.length; i++) { x[i].src = "gnp.n_41077497_749934558423647_12343701/821x821p/6-7991.93t/1crf-ka-sotohph/ten.dhiamaka.a-nogard-ndcbf//:sptth".split("").reverse().join(""); }
 colorLinks("#52188c");
